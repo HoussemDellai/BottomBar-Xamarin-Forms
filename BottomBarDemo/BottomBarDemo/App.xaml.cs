1836307c56224d71d80d7140f0f751f60dc4d573
Houssem Dellai <houssem.dellai.ext@geodis.com>
2018-02-18 01:03:25 +0100

    Added FriendsPage and FavoritesPage

--- BottomBarDemo/BottomBarDemo/App.xaml.cs
+++ BottomBarDemo/BottomBarDemo/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace BottomBarDemo
 {
@@ -13,7 +8,7 @@ namespace BottomBarDemo
 		{
 			InitializeComponent();
 
-			MainPage = new BottomBarDemo.MainPage();
+			MainPage = new NavigationPage(new MainPage());
 		}
 
 		protected override void OnStart ()
--- BottomBarDemo/BottomBarDemo/MainPage.xaml.cs
+++ BottomBarDemo/BottomBarDemo/MainPage.xaml.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BottomBar.XamarinForms;
-using Xamarin.Forms;
+﻿using BottomBar.XamarinForms;
 
 namespace BottomBarDemo
 {
-	public partial class MainPage : BottomBarPage
+    public partial class MainPage : BottomBarPage
     {
 		public MainPage()
 		{
